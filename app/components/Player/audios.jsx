39b5f9cf96b6db4f4121a02c699f4184c32ee603
Chetanya hindwan <117354715+chetanya04@users.noreply.github.com>
2023-12-15 14:22:05 +0530

    Add files via upload

---
+++ app/components/Player/audios.jsx
@@ -0,0 +1,15 @@
+"use client"
+exports.songsdata = [
+    {
+        "title": "The Neverwritten Role Playing Game",
+        "url": "https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3"
+    },
+    {
+        "title": "Linking Park - In the end",
+        "url": "https://cdn.simplecast.com/audio/cae8b0eb-d9a9-480d-a652-0defcbe047f4/episodes/af52a99b-88c0-4638-b120-d46e142d06d3/audio/500344fb-2e2b-48af-be86-af6ac341a6da/default_tc.mp3"
+    },
+    {
+        "title": "Codeskulptor demos",
+        "url": "https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3"
+    }
+]
---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "./components/Player/Header"
+import App from "./components/Player/page"
+export default function Home() {
+  return (
+    <>
+    <Header/>
+    <App/>
+    </>
+  )}
